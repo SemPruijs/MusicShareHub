9d27ccdd276275b17ae6a56a04e4219560791078
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-09-19 17:22:25 +0200

    You cannot have more than 3 links now

--- JS/GenerateLink.js
+++ JS/GenerateLink.js
@@ -1,4 +1,7 @@
+let streamingServices = ["Apple", "Spotify", "Youtube"];
+
 function generateLink(songName, platform) {
+    
     var spotifyLink = "https://open.spotify.com/search/";
     var appleMusicLink = "https://music.apple.com/us/search?term=";
     var youtubeLink = "https://www.youtube.com/results?search_query=";
@@ -18,14 +21,22 @@ function generateLink(songName, platform) {
 }
 
 function DrawPage() {
+    ClearLinksContainer();
     let container = document.getElementById("linksContainer");
-
-    let streamingServices = ["Apple", "Spotify", "Youtube"];
-
+    
     for (let i = 0; i < streamingServices.length; i++) {
         var link = document.createElement("a");
         link.href = generateLink(document.getElementById("inputField").value.toString(), streamingServices[i]);
         link.innerHTML = streamingServices[i];
+        link.id = streamingServices[i];
         container.appendChild(link);
+    }
+}
+
+function ClearLinksContainer() {
+    if (document.getElementById("Apple") != null) {
+        for (let i = 0; i < streamingServices.length; i++) {
+            document.getElementById(streamingServices[i]).innerHTML = ""
+        }   
     }
 }
