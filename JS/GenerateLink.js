70bc7fb54b22553134b5b0171f24059181004bb0
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-09-19 16:46:27 +0200

    first commit

---
+++ JS/GenerateLink.js
@@ -0,0 +1,44 @@
+function generateLink(songName, platform) {
+    var spotifyLink = "https://open.spotify.com/search/";
+    var appleMusicLink = "https://music.apple.com/us/search?term=";
+    var youtubeLink = "https://www.youtube.com/results?search_query=";
+    
+    spotifyLink += songName.replaceAll(/ /g,"%20");
+    appleMusicLink += songName.replaceAll(/ /g,"%20");
+    youtubeLink += songName.replaceAll(/ /g,"+");
+    
+    switch (platform) {
+        case "Spotify":
+            return spotifyLink;
+        case "Apple":
+            return appleMusicLink;
+        case "Youtube":
+            return youtubeLink;
+    } 
+}
+
+// function DrawPage() {
+//     let container = document.getElementById("linksContainer");
+//    
+//     let streamingServices = ["Apple", "Spotify", "Youtube"];
+//    
+//     for (let i = 0; i < streamingServices.length; i++) {
+//         var link = document.createElement("a");
+//         link.href = generateLink("Daft Punk", streamingServices[i]);
+//         link.innerHTML = streamingServices[i];
+//         container.appendChild(link);   
+//     }
+// }
+
+window.onload = function () {
+    let container = document.getElementById("linksContainer");
+
+    let streamingServices = ["Apple", "Spotify", "Youtube"];
+
+    for (let i = 0; i < streamingServices.length; i++) {
+        var link = document.createElement("a");
+        link.href = generateLink("Daft Punk", streamingServices[i]);
+        link.innerHTML = streamingServices[i];
+        container.appendChild(link);
+    }
+}
