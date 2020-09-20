0d870c762db0235998f88150323ac672206d8cdc
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-09-20 09:38:50 +0200

    Change var into const and let in GenerateLink.js

--- JS/GenerateLink.js
+++ JS/GenerateLink.js
@@ -4,13 +4,11 @@ let Images = ["./Images/Spotify.jpeg", "./Images/AppleMusic.jpeg", "./Images/you
 let ImagesAlt = ["Spotify logo", "Apple music logo", "Youtube logo"]
 
 
-
 function generateLink(songName, platform) {
-    
-    var spotifyLink = "https://open.spotify.com/search/";
-    var appleMusicLink = "https://music.apple.com/us/search?term=";
-    var youtubeLink = "https://www.youtube.com/results?search_query=";
-    
+    let spotifyLink = "https://open.spotify.com/search/";
+    let appleMusicLink = "https://music.apple.com/us/search?term=";
+    let youtubeLink = "https://www.youtube.com/results?search_query=";
+
     spotifyLink += songName.replaceAll(/ /g,"%20");
     appleMusicLink += songName.replaceAll(/ /g,"%20");
     youtubeLink += songName.replaceAll(/ /g,"+");
@@ -25,24 +23,28 @@ function generateLink(songName, platform) {
     } 
 }
 
+function GetUrlPutInInputField() {
+    
+}
+
 function DrawPage() {
     ClearLinksContainer();
     let LinkContainer = document.getElementById("linksContainerForDivs");
     
     for (let i = 0; i < streamingServices.length; i++) {
         //Create container for link
-        var container = document.createElement("div");
+        const container = document.createElement("div");
         container.className = "Container";
         LinkContainer.appendChild(container);
         
         //Create link
-        var link = document.createElement("a");
+        const link = document.createElement("a");
         link.href = generateLink(document.getElementById("inputField").value.toString(), streamingServices[i]);
         link.id = streamingServices[i];
         container.appendChild(link);
         
         //Create image
-        var image = document.createElement("img");
+        const image = document.createElement("img");
         image.src = Images[i];
         image.alt = ImagesAlt[i];
         link.appendChild(image);
